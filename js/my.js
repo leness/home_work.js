11b568eeebe4854ffd7ebf55ebc63bf022e8d572
Olena Stadnyk <lenesx@gmail.com>
2020-10-17 11:19:09 +0300

    Решение задачи

--- js/my.js
+++ js/my.js
@@ -1,101 +1,149 @@
-function getAllPropValues(array, prop) {
-  "use strict";
-  // Write code under this line
-  const names = [];
-  const quantitys = [];
+/*
+ * Функция обратного вызова (callback)
+ * - Функция может принимать другие функции как параметры
+ * - Функция которая передаётся другой функции как аргумент называетс
+ *   «функцией обратного (отложенного) вызова» (callback-функция)
+ * - Функция которая принимает другую функцию как параметр
+ *   или возвращает функцию как результат своей работы называется «функцией высшего порядка»
+ */
 
-  for (const props of array) {
-    
-    names.push(props.name);
-    quantitys.push(props.quantity);
-  }
-  return names;
-  return quantitys;
-}
+// const fnA = function (message, callback) {
+//     console.log(message);
 
-// Объекты и ожидаемый результат
-const products = [
-  { name: "Радар", price: 1300, quantity: 4 },
-  { name: "Радар", price: 1280, quantity: 2 },
-  { name: "Радар", price: 1320, quantity: 1 },
-  { name: "Сканер", price: 2700, quantity: 1 },
-  { name: "Сканер", price: 2500, quantity: 3 },
-  { name: "Дроид", price: 400, quantity: 7 },
-  { name: "Захват", price: 1200, quantity: 2 },
-];
+//     console.log(callback);
+//     callback(100);
+// };
 
-console.log(getAllPropValues(products, "name"));
-// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+// const fnB = function (number) {
+//     console.log('Это лог при вызове fnB', number);
+// };
 
-console.log(getAllPropValues(products, "quantity"));
-// [4, 2, 1, 1, 3, 7, 2]
+// fnA('qweqwe', fnB);
 
-console.log(getAllPropValues(products, "category"));
-//  []
-// function getAllPropValues(array, prop) {
-//   "use strict";
-//   // Write code under this line
-//   function getAllPropValues(array, prop) {
-//     "use strict";
-//     // Write code under this line
-//     for (const props of array) {
-//       console.log(array);
-//       console.log(array.name);
+/*
+ * функция doMath(a, b, callback)
+ */
+
+const doMath = function (a, b, callback) {
+    const result = callback(a, b);
+
+    console.log(result);
+};
+
+// doMath(2, 3, function (x, y) {
+//     return x + y;
+// });
+
+// doMath(10, 8, function (x, y) {
+//     return x - y;
+// });
+
+/*
+ * Отложенный вызов: регистрация событий
+ */
+
+const buttonRef = document.querySelector('.js-button');
+
+const handleBtnClick = function () {
+    console.log('Клик по кнопке ' + Date.now());
+};
+
+// buttonRef.addEventListener('click', handleBtnClick);
+
+/*
+ * Отложенный вызов: геолокация
+ */
+
+const onGetPositionSuccess = function (position) {
+    console.log('Это вызов onGetPositionSuccess');
+    console.log(position);
+};
+
+const onGetPositionError = function (error) {
+    console.log('Это вызов onGetPositionError');
+    console.log(error);
+};
+
+// window.navigator.geolocation.getCurrentPosition(
+//     onGetPositionSuccess,
+//     onGetPositionError,
+// );
+
+/*
+ * Отложенный вызов: интервалы
+ */
+
+const callback = function () {
+    console.log('Через 2 секунды внутри колбека в таймауте');
+};
+
+// console.log('В коде перед таймаутом');
+// setTimeout(callback, 2000);
+// console.log('В коде после таймаута');
+
+/*
+ * Отложенный вызов: http-запрос
+ * - API URL: https://pokeapi.co/api/v2/pokemon
+ */
+
+const onRequestSuccess = function (response) {
+    console.log(
+        'Вызов функции onRequestSuccess после успешного ответа бекенда',
+    );
+    console.log(response);
+};
+
+// fetch('https://pokeapi.co/api/v2/pokemon')
+//     .then(res => res.json())
+//     .then(onRequestSuccess);
+
+/*
+ * Фильтр
+ */
+
+// const filter = function (array, test) {
+//     const filteredArray = [];
+
+//     for (const el of array) {
+//         console.log(el);
+//         const passed = test(el);
+
+//         if (passed) {
+//             filteredArray.push(el);
+//         }
 //     }
-//   }
 
-//   // Объекты и ожидаемый результат
-//   const products = [
-//     { name: "Радар", price: 1300, quantity: 4 },
-//     { name: "Радар", price: 1280, quantity: 2 },
-//     { name: "Радар", price: 1320, quantity: 1 },
-//     { name: "Сканер", price: 2700, quantity: 1 },
-//     { name: "Сканер", price: 2500, quantity: 3 },
-//     { name: "Дроид", price: 400, quantity: 7 },
-//     { name: "Захват", price: 1200, quantity: 2 },
-//   ];
+//     return filteredArray;
+// };
 
-//   console.log(getAllPropValues(products, "name"));
-//   // ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+// // 1. надо передать функцию
+// // 2. функция получает элемент массива
+// // 3. если элемент массива удовлетворяет условию то функция вернет true
+// // 3. если элемент массива НЕ удовлетворяет условию то функция вернет false
 
-//   console.log(getAllPropValues(products, "quantity"));
-//   // [4, 2, 1, 1, 3, 7, 2]
+// const callback1 = function (value) {
+//     return value >= 3;
+// };
 
-//   console.log(getAllPropValues(products, "category"));
-//   //  []
+// const r1 = filter([1, 2, 3, 4, 5], callback1);
+// console.log(r1);
 
-//   //   for (let i = 0; i < products.length; i += 1) {
-//   //     let arr = Object.entries(products[i]);
+// const callback2 = function (value) {
+//     return value <= 4;
+// };
 
-//   //     products[i] = array;
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
+// console.log(r2);
 
-//   //     const index = products[i][0].indexOf(prop);
-//   //     let newArr = [];
-
-//   //     newArr.push(products[i][0][1]);
-
-//   //     console.log(newArr);
-//   //   }
-
-//   //   return newArr;
-// }
-
-// // Объекты и ожидаемый результат
-// const products = [
-//   { name: "Радар", price: 1300, quantity: 4 },
-//   { name: "Радар", price: 1280, quantity: 2 },
-//   { name: "Радар", price: 1320, quantity: 1 },
-//   { name: "Сканер", price: 2700, quantity: 1 },
-//   { name: "Сканер", price: 2500, quantity: 3 },
-//   { name: "Дроид", price: 400, quantity: 7 },
-//   { name: "Захват", price: 1200, quantity: 2 },
+// const fruits = [
+//     { name: 'apples', quantity: 200, isFresh: true },
+//     { name: 'grapes', quantity: 150, isFresh: false },
+//     { name: 'bananas', quantity: 100, isFresh: true },
 // ];
 
-// console.log(getAllPropValues(products, "name"));
-// // ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+// const getFruitsWithQuantity = function (fruit) {
+//     return fruit.quantity >= 120;
+// };
 
-// console.log(getAllPropValues(products, "quantity"));
-// // [4, 2, 1, 1, 3, 7, 2]
-
-// console.log(getAllPropValues(products, "category"));
-// //  []
+// const r3 = filter(fruits, getFruitsWithQuantity);
+// console.log(r3);
