3c3bf0a10d437a41473f0cb5895f8b8096f8f1b3
Olena Stadnyk <lenesx@gmail.com>
2020-10-18 21:03:51 +0300

    Добавляет задачи 4 модуля

--- js/task-43.js
+++ js/task-43.js
@@ -10,38 +10,38 @@
 // в зависимости от того какая именно из трех функция(add, mult, sub)
 // будет передана в качестве cb.
 
-const add = (accum, element) => accum + element;
-const mult = (accum, element) => accum * element;
-const sub = (accum, element) => accum - element;
+// const add = (accum, element) => accum + element;
+// const mult = (accum, element) => accum * element;
+// const sub = (accum, element) => accum - element;
 
-function reduceArray(array, cb, initial) {
-  'use strict';
-  let i;
-  let accum;
-  if(arguments.length >= 3) {
-    accum = initial;
-    i = 0;
-  }
-  if(arguments.length === 2) {
-    accum = array[0];
-    i = 1;
-  }
-  for(i; i < array.length; i += 1) {
-    const element = array[i];
-    // Write code under this line
-    accum = cb(accum, element)
-  }
-  return accum;
-  accum = cb(accum, element)
-}
+// function reduceArray(array, cb, initial) {
+//   'use strict';
+//   let i;
+//   let accum;
+//   if(arguments.length >= 3) {
+//     accum = initial;
+//     i = 0;
+//   }
+//   if(arguments.length === 2) {
+//     accum = array[0];
+//     i = 1;
+//   }
+//   for(i; i < array.length; i += 1) {
+//     const element = array[i];
+//     // Write code under this line
+//     accum = cb(accum, element)
+//   }
+//   return accum;
+//   accum = cb(accum, element)
+// }
 
-const arr  = [1,2,3,4,5];
+// const arr  = [1,2,3,4,5];
 
-console.log(reduceArray(arr, add)); // 15
-console.log(reduceArray(arr, add, 10)); // 25
+// console.log(reduceArray(arr, add)); // 15
+// console.log(reduceArray(arr, add, 10)); // 25
 
-console.log(reduceArray(arr, mult)); // 120
-console.log(reduceArray(arr, mult, 10)); // 1200
+// console.log(reduceArray(arr, mult)); // 120
+// console.log(reduceArray(arr, mult, 10)); // 1200
 
-console.log(reduceArray(arr, sub)); // -13
-console.log(reduceArray(arr, sub, 10)); // -5
+// console.log(reduceArray(arr, sub)); // -13
+// console.log(reduceArray(arr, sub, 10)); // -5
